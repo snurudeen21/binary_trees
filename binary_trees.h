13b237aedab63b1005b0bd3872581b29ab8254b7
mukarama-farouk <faroukmukarama@gmail.com>
2023-08-30 02:15:43 -0700

    task 0

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
 #ifndef BINARY_TREES_H
-#define BINARY _TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
 #include <stdio.h>
@@ -33,6 +33,8 @@ typedef struct binary_tree_s heap_t;
 void binary_tree_print(const binary_tree_t *);
 
 /* Task function prototypes */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 
 
