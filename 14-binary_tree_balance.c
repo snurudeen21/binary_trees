975d0bb784750be94c068d133f2f0ec879d9de10
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-03-17 09:08:36 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -19,3 +19,21 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (h_l - h_r);
 }
+
+/**
+ * binary_tree_height - return the size of a binary tree
+ * @tree: pointer to root node
+ * Return: Height of tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL)
+	return (0);
+
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (left_height > right_height ? left_height : right_height);
+}
