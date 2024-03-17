21517704199579f0f8e0c727636a6667ce096c16
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-03-17 08:33:51 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -16,12 +16,12 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-    if (tree->left != NULL || tree->right != NULL)
-    {
-        count = 1;
-    }
-    count += binary_tree_nodes(tree->left);
-    count += binary_tree_nodes(tree->right);
+	if (tree->left != NULL || tree->right != NULL)
+	{
+		count = 1;
+	}
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
 
-    return (count);
+	return (count);
 }
