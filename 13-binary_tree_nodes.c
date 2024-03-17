b051cd13710405c718932a10b606fd655faf976a
Sulaimon Nurudeen Adisa <snurudeen21@gmail.com>
2024-03-17 07:36:57 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,14 +1,22 @@
-#include "
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts the nodes in the binary tree
+ * @tree: a pointer to the root node of the tree
+ * to count the number of leaves
+ *
+ * Return: 0 if tree is NULL
+ */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    /* Base case: If tree is NULL, return 0 */
-    if (tree == NULL) {
+    if (tree == NULL)
+    {
         return 0;
     }
 
-    /* Recursive case: Count nodes with at least one child */
     size_t count = 0;
-    if (tree->left != NULL || tree->right != NULL) {
+    if (tree->left != NULL || tree->right != NULL)
+    {
         count = 1;
     }
     count += binary_tree_nodes(tree->left);
